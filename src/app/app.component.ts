03400af527b785d914aca0d19d8819d93ba34b52
ahlem25 <ahlem.cherni112@gmail.com>
2025-01-16 22:17:30 +0100

    test angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
-
+import { Component,OnInit, ViewChild ,AfterViewInit} from '@angular/core';
+import { EmployeesService } from './employees.service';
+import {MatPaginator} from "@angular/material/paginator";
+import {MatTableDataSource} from "@angular/material/table";
+import {MatSort, Sort} from '@angular/material/sort';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: false,
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'test-technique';
+export class AppComponent implements OnInit,AfterViewInit{
+
+  displayedColumns: string[] = ['id', 'age', 'dob', 'email', 'salary', 'address','lastName','firstName','contactNumber'];
+  totalEmployees: number = 0;
+  pageSize: number = 10;
+  currentPage: number = 0;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+  dataSource: MatTableDataSource<any> = new MatTableDataSource();
+
+   constructor(private employeesService:EmployeesService){
+
+   }
+
+   ngOnInit(): void {
+    this.employeesService.getEmployees().subscribe((response:any)=>{
+      //console.log(response);
+      this.totalEmployees = response.length;
+
+      this.dataSource.data = response;
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+    });
+
+    
+   }
+   ngAfterViewInit() {
+    //this.dataSource.sort = this.sort;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {MatTableModule} from "@angular/material/table";
+import {MatPaginatorModule} from "@angular/material/paginator";
+import {MatButtonModule} from "@angular/material/button";
+import {MatSortModule} from "@angular/material/sort";
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatButtonModule,
+    MatSortModule
   ],
   providers: [
-    provideClientHydration(withEventReplay())
+    provideClientHydration(withEventReplay()),
+    provideAnimationsAsync()
   ],
   bootstrap: [AppComponent]
 })
